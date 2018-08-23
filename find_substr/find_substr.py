2d2975d36b6250a7af58a8fc20134cb837b95d43
Edward Young <1564630+edwardyoung@users.noreply.github.com>
2018-08-23 02:52:06 +0000

    Add comments

--- find_substr/find_substr.py
+++ find_substr/find_substr.py
@@ -14,16 +14,21 @@ def call_api(input):
 count = 0
 old_string = ""
 bad_str = ""
+
+# Remove characters from the beginning until the api call returns false
 while call_api(input_str):
     old_string = input_str
     input_str = input_str[1:]
     count += 1
     print str(count) + " " + input_str
-    
+
+# Start by using the last string that caused the api call to return false
+# Remove characters from the end until the api call returns false
 while call_api(old_string):
     bad_str = old_string
     old_string = old_string[:-1]
     count += 1
     print str(count) + " " + old_string
 
+# Print the substring that causes the api call to fail
 print bad_str
